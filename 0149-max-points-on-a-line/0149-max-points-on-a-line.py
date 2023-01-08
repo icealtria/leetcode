99cb2c0321ddc6056e308139c9553ce5e28cfa39
icealtria <icealtria@gmail.com>
2023-01-08 11:14:06 +0800

    Time: 103 ms (83.38%), Space: 14 MB (75.79%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        max_points = 0
+        for i in range(len(points)):
+            same_x_points = 1
+            slope_count = {}
+            for j in range(len(points)):
+                if i == j:
+                    continue
+                point1 = points[i]
+                point2 = points[j]
+                if point1[0] == point2[0]:
+                    same_x_points += 1
+                else:
+                    slope = (point1[1] - point2[1]) / (point1[0] - point2[0])
+                    if slope not in slope_count:
+                        slope_count[slope] = 1
+                    else:
+                        slope_count[slope] += 1
+            max_points = max(max_points, same_x_points)
+            for count in slope_count.values():
+                max_points = max(max_points, count + 1)
+        return max_points
