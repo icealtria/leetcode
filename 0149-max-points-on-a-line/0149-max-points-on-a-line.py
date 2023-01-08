58ee31550b0dd6c99e7146352dde16c44b0c0f8d
icealtria <icealtria@gmail.com>
2023-01-08 11:15:57 +0800

    Time: 96 ms (85.83%), Space: 14 MB (53.74%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -14,10 +14,10 @@ class Solution:
                 else:
                     slope = (point1[1] - point2[1]) / (point1[0] - point2[0])
                     if slope not in slope_count:
-                        slope_count[slope] = 1
+                        slope_count[slope] = 2
                     else:
                         slope_count[slope] += 1
             max_points = max(max_points, same_x_points)
             for count in slope_count.values():
-                max_points = max(max_points, count + 1)
+                max_points = max(max_points, count)
         return max_points
