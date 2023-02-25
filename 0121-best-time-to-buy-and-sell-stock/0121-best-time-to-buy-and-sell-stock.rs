0d40685159869be583bb55b768d91befa7ee4bc9
icealtria <icealtria@gmail.com>
2023-02-25 10:24:04 +0800

    Time: 13 ms (72.23%), Space: 2.9 MB (67.46%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rs
@@ -1,14 +1,11 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let n = prices.len();
-        let mut max_prices = vec![0; n];
-        max_prices[n-1] = prices[n-1];
-        for i in (0..n-1).rev() {
-            max_prices[i] = max_prices[i+1].max(prices[i]); 
-        }
+        let mut buy_at_price = i32::MAX;
         let mut max_profit = 0;
         for i in 0..n {
-            max_profit = max_profit.max(max_prices[i] - prices[i]); 
+            buy_at_price = buy_at_price.min(prices[i]);
+            max_profit = max_profit.max(prices[i] - buy_at_price);
         }
         max_profit
     }
